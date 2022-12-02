dbf78b8c9125c96c1914f6db1b8293421c7f4158
LucaFaedda <lucakarl91@gmail.com>
2022-12-02 17:17:10 +0100

    aggiustato con solo while

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ let i = 0
 let listItem = ''
 
 
-do{
-    
+while(i < lista_anime.length ){
     console.log(lista_anime[i])  // mi stampa ogni singolo elemento della lista
-
+    
     let listItem = lista_anime[i]
     listItem = `<li class = "lista" >${listItem}</li>`
     listContain.innerHTML += listItem
     i++ // Mi serve per incrementare ogni volta il valore sennò va in loop
     
-}while(i < lista_anime.length  )
+    
+}
