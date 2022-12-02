a9d83b94c8f889ffcea567ed05d54721f9cc9ef0
LucaFaedda <lucakarl91@gmail.com>
2022-12-02 14:45:09 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ const lista_anime = ['HunterXHunter','BlueLock','OnePiece', 'GodOfTowes', 'FireF
 const listContain = document.querySelector('.list-unstiled')
 
 let i = 0
+let listItem = ''
 
-while(i < lista_anime.length){
+
+do{
     
     console.log(lista_anime[i])  // mi stampa ogni singolo elemento della lista
 
-    let listItem = document.createElement('li')
-    listItem.innerText = item
-    listContain.append(listItem)
-
+    let listItem = lista_anime[i]
+    listItem = `<li class = "lista" >${listItem}</li>`
+    listContain.innerHTML += listItem
+    i++ // Mi serve per incrementare ogni volta il valore sennò va in loop
     
-
-}
-
+}while(i < lista_anime.length  )
